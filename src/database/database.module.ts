deebb2b7f91224e718b006b1bc61b1d324c153c2
Kevin Jimenez <kjimenez@creditu.com>
2023-07-30 20:59:08 -0500

    feat: db prod

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,16 +7,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        database: configService.get('DB_NAME'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'prod';
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: +configService.get('DB_PORT'),
+          database: configService.get('DB_NAME'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
     }),
   ],
   exports: [TypeOrmModule],
